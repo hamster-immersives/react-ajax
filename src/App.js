0882d5f2ab58015b8105708e300789b6d98bc6c3
pak <pak@1916.lan>
2019-02-01 12:51:25 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    data: [],
+    city: ''
+  }
+
+  componentDidMount() {
+    
+    axios.get('https://developers.zomato.com/api/v2.1/cities?q=new%20york', { 'headers': { 
+      'Accept': 'application/json',
+      'user-key': '4fc6a8d12cfad4fa96a78af7e0712108'
+        }})
+        .then(hamster => {
+          this.setState({
+            data: hamster.data.location_suggestions
+          })
+
+        })
+        .catch(error => {
+        console.log(error)
+        })
+  }
+
+  handleUserInput = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  } 
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios.get(`https://developers.zomato.com/api/v2.1/cities?q=${this.state.city}`, { 'headers': { 
+      'Accept': 'application/json',
+      'user-key': '4fc6a8d12cfad4fa96a78af7e0712108'
+        }})
+        .then(hamster => {
+          this.setState({
+            data: hamster.data.location_suggestions
+          })
+
+        })
+        .catch(error => {
+        console.log(error)
+        })
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.data.map((element, index) => {
+            return (
+              <div key={index}> 
+                  {element.name}
+              </div>
+            )
+          })
+        }
+
+
+        <form onSubmit={this.handleSubmit}>
+          <input name="city" onChange={this.handleUserInput}/>
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
